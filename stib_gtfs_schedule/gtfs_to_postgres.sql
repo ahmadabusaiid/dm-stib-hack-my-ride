74d0e44969562f42b548cc2c4ad280c3a0bd3157
Alaa Almutawa <alaaalmutawa@Alaas-MacBook-Pro.local>
2022-11-08 01:42:33 +0100

    update script to properly handled gtfs time-ref: mobilitydb doc

--- stib_gtfs_schedule/gtfs_to_postgres.sql
+++ stib_gtfs_schedule/gtfs_to_postgres.sql
@@ -1,186 +1,116 @@
+--Ref: https://docs.mobilitydb.com/MobilityDB-workshop/master/ch04.html
+--Adjusted to fit our use case 
+
 DROP TABLE IF EXISTS agency;
 
-
 CREATE TABLE agency (
-  agency_id VARCHAR(255) PRIMARY KEY,
-  agency_name VARCHAR(255),
-  agency_url VARCHAR(255),
-  agency_timezone VARCHAR(50),
-  agency_lang VARCHAR(255),
-  agency_phone VARCHAR(50)
+  agency_id text DEFAULT '',
+  agency_name text DEFAULT NULL,
+  agency_url text DEFAULT NULL,
+  agency_timezone text DEFAULT NULL,
+  agency_lang text DEFAULT NULL,
+  agency_phone text DEFAULT NULL,
+  CONSTRAINT agency_pkey PRIMARY KEY (agency_id)
 );
 
 DROP TABLE IF EXISTS calendar;
 
 CREATE TABLE calendar (
-  service_id INT,
-  monday SMALLINT,
-  tuesday SMALLINT,
-  wednesday SMALLINT,
-  thursday SMALLINT,
-  friday SMALLINT,
-  saturday SMALLINT,
-  sunday SMALLINT,
-  start_date VARCHAR(8),
-  end_date VARCHAR(8)
+  service_id text,
+  monday int NOT NULL,
+  tuesday int NOT NULL,
+  wednesday int NOT NULL,
+  thursday int NOT NULL,
+  friday int NOT NULL,
+  saturday int NOT NULL,
+  sunday int NOT NULL,
+  start_date date NOT NULL,
+  end_date date NOT NULL,
+  CONSTRAINT calendar_pkey PRIMARY KEY (service_id)
 );
-
-CREATE INDEX service_id_on_calendar ON calendar (service_id);
+CREATE INDEX calendar_service_id ON calendar (service_id);
 
 DROP TABLE IF EXISTS calendar_dates;
 
 CREATE TABLE calendar_dates (
-  service_id INT,
-  date VARCHAR(8),
-  exception_type INT
+  service_id text,
+  date date NOT NULL,
+  exception_type int
 );
-
-CREATE INDEX service_id_on_dates ON calendar_dates (service_id);
---CREATE INDEX exception_type ON calendar_dates (exception_type); --why index?
-
---DROP TABLE IF EXISTS fare_attributes; --fare_attributes table doesnt exist in STIB data
-
--- SQLINES LICENSE FOR EVALUATION USE ONLY
-/*CREATE TABLE fare_attributes (
-  fare_id INT,
-  price DECIMAL(9,6),
-  currency_type VARCHAR(8),
-  payment_method INT,
-  transfers INT
-);*/
-
---CREATE INDEX fare_id ON fare_attributes (fare_id);
-
---DROP TABLE IF EXISTS fare_rules; --fare_rules table doesnt exist in STIB data 
-
--- SQLINES LICENSE FOR EVALUATION USE ONLY
-/*CREATE TABLE fare_rules (
-  fare_id INT,
-  route_id INT
-);*/
-
---CREATE INDEX fare_id ON fare_rules (fare_id);
---CREATE INDEX route_id ON fare_rules (route_id);
-
+CREATE INDEX calendar_dates_dateidx ON calendar_dates (date);
 
 DROP TABLE IF EXISTS routes;
 
---route_id,
---route_short_name,
---route_long_name,
---route_desc,
---route_type,
---route_url,
---route_color,
---route_text_color
-
 CREATE TABLE routes (
-  route_id INT PRIMARY KEY,
-  route_short_name VARCHAR(50),
-  route_long_name VARCHAR(255),
-  route_desc VARCHAR(255),
-  route_type INT,
-  route_url VARCHAR(255),
-  route_color VARCHAR(255),
-  route_text_color VARCHAR(255)
-
+  route_id text,
+  route_short_name text DEFAULT '',
+  route_long_name text DEFAULT '',
+  route_desc text DEFAULT '',
+  route_type int,
+  route_url text,
+  route_color text,
+  route_text_color text,
+  CONSTRAINT routes_pkey PRIMARY KEY (route_id)
 );
-
-CREATE INDEX route_type ON routes (route_type);
 
 DROP TABLE IF EXISTS shapes;
 
---shape_id,
---shape_pt_lat,
---shape_pt_lon,
---shape_pt_sequence
-
 CREATE TABLE shapes (
-  shape_id VARCHAR(50), --there are duplicates, we cant have this as a key
-  shape_pt_lat DECIMAL(9,6),
-  shape_pt_lon DECIMAL(9,6),
-  shape_pt_sequence INT
+  shape_id text NOT NULL,
+  shape_pt_lat double precision NOT NULL,
+  shape_pt_lon double precision NOT NULL,
+  shape_pt_sequence int NOT NULL
 );
-
-CREATE INDEX shape_id ON shapes (shape_id); --we cant have a pkey on shape_id, thus, we put an index 
-
-DROP TABLE IF EXISTS stop_times;
-
---trip_id,
---arrival_time,
---departure_time,
---stop_id,
---stop_sequence,
---pickup_type,
---drop_off_type
-
-CREATE TABLE stop_times (
-  trip_id BIGSERIAL, --112387248235954071 .. pretty long using INT or SERIAL didnt work
-  arrival_time VARCHAR(8),
-  departure_time VARCHAR(8),
-  stop_id VARCHAR(8), -- changed to varchar -> it has int and char
-  stop_sequence INT,
-  pickup_type INT,
-  drop_off_type INT
-);
-
-CREATE INDEX trip_id ON stop_times (trip_id);
-CREATE INDEX stop_id ON stop_times (stop_id);
-CREATE INDEX stop_sequence ON stop_times (stop_sequence);
+CREATE INDEX shapes_shape_key ON shapes (shape_id);
 
 DROP TABLE IF EXISTS stops;
 
---stop_id,
---stop_code,
---stop_name,
---stop_desc,
---stop_lat,
---stop_lon,
---zone_id,
---stop_url,
---location_type,
---parent_station
-
 CREATE TABLE stops (
-  stop_id VARCHAR(8) PRIMARY KEY, --choose varchar cause: 0470F 
-  stop_code INT,
-  stop_name VARCHAR(255),
-  stop_desc VARCHAR(255),
-  stop_lat DECIMAL(9,6),
-  stop_lon DECIMAL(9,6),
-  zone_id INT,
-  stop_url VARCHAR(255),
-  location_type INT,
-  parent_station INT
+  stop_id text,
+  stop_code text,
+  stop_name text DEFAULT NULL,
+  stop_desc text DEFAULT NULL,
+  stop_lat double precision,
+  stop_lon double precision,
+  zone_id text,
+  stop_url text,
+  location_type integer,
+  parent_station integer,
+  CONSTRAINT stops_pkey PRIMARY KEY (stop_id)
 );
 
-CREATE INDEX stop_lat ON stops (stop_lat); --do we need this?
-CREATE INDEX stop_lon ON stops (stop_lon); --do we need this?
+DROP TABLE IF EXISTS stop_times;
+
+CREATE TABLE stop_times (
+  trip_id text NOT NULL,
+  -- Check that casting to time interval works.
+  arrival_time interval CHECK (arrival_time::interval = arrival_time::interval),
+  departure_time interval CHECK (departure_time::interval = departure_time::interval),
+  stop_id text,
+  stop_sequence int,
+  pickup_type INT,
+  drop_off_type int,
+  CONSTRAINT stop_times_pkey PRIMARY KEY (trip_id, stop_sequence)
+);
+
+CREATE INDEX stop_times_key ON stop_times (trip_id, stop_id);
+CREATE INDEX arr_time_index ON stop_times (arrival_time);
+CREATE INDEX dep_time_index ON stop_times (departure_time);
 
 DROP TABLE IF EXISTS trips;
 
---route_id,
---service_id,
---trip_id,
---trip_headsign,
---direction_id,
---block_id,
---shape_id
-
 CREATE TABLE trips (
-  route_id INT,
-  service_id INT,
-  trip_id BIGSERIAL PRIMARY KEY,
-  trip_headsign VARCHAR(255),
-  direction_id SMALLINT,
-  block_id INT,
-  shape_id VARCHAR(50)
+  route_id text NOT NULL,
+  service_id text NOT NULL,
+  trip_id text NOT NULL,
+  trip_headsign text,
+  direction_id int,
+  block_id text,
+  shape_id text,
+  CONSTRAINT trips_pkey PRIMARY KEY (trip_id)
 );
+CREATE INDEX trips_trip_id ON trips (trip_id);
 
-CREATE INDEX route_id ON trips (route_id);
-CREATE INDEX service_id_on_trips ON trips (service_id);
-CREATE INDEX direction_id ON trips (direction_id);--do we need this?
-CREATE INDEX shape_id ON trips (shape_id);
 
 COPY agency FROM '/Users/alaaalmutawa/Documents/INFO423/Project/Project Data-20221101/gtfs3Sept/agency.txt' ( FORMAT CSV, HEADER);
 COPY calendar FROM '/Users/alaaalmutawa/Documents/INFO423/Project/Project Data-20221101/gtfs3Sept/calendar.txt' ( FORMAT CSV, HEADER);
@@ -191,6 +121,3 @@ COPY stop_times FROM '/Users/alaaalmutawa/Documents/INFO423/Project/Project Data
 COPY stops FROM '/Users/alaaalmutawa/Documents/INFO423/Project/Project Data-20221101/gtfs3Sept/stops.txt' ( FORMAT CSV, HEADER);
 --COPY translations FROM '/Users/alaaalmutawa/Documents/INFO423/Project/Project Data-20221101/gtfs3Sept/translations.txt' ( FORMAT CSV, HEADER);
 COPY trips FROM '/Users/alaaalmutawa/Documents/INFO423/Project/Project Data-20221101/gtfs3Sept/trips.txt' ( FORMAT CSV, HEADER);
-
---did not create a translation table 
-
